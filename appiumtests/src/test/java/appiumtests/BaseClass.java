8778ee8f8c8af562f7c4e605ba9f7cce124a9b79
kelvin <kelvinityavyar@gmail.com>
2024-11-12 07:21:29 -0800

    implemented signUp test and modularized the code base for reusability

---
+++ appiumtests/src/test/java/appiumtests/BaseClass.java
@@ -0,0 +1,38 @@
+// BaseTest.java
+package appiumtests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class BaseClass {
+
+    protected static AppiumDriver driver;
+
+    public void setup() {
+        try {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("appium:appActivity", "com.example.mbl.MainActivity");
+            capabilities.setCapability("appium:appPackage", "com.example.mbl");
+            capabilities.setCapability("appium:automationName", "UiAutomator2");
+            capabilities.setCapability("appium:deviceName", "TECNO SPARK 20");
+            capabilities.setCapability("appium:platformVersion", "12");
+            capabilities.setCapability("appium:udid", "192.168.1.101:5555");
+
+            driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), capabilities);
+            System.out.println("Application started");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            System.out.println("The URL for Appium server is malformed.");
+        }
+    }
+
+
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
--- appiumtests/src/test/java/appiumtests/SignupTest.java
+++ appiumtests/src/test/java/appiumtests/SignupTest.java
@@ -1,5 +1,38 @@
 package appiumtests;
 
-public class SignupTest {
+import org.openqa.selenium.WebElement;
+import io.appium.java_client.AppiumBy;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test; 
 
+public class SignupTest extends BaseClass {
+
+    private AppNavigationHelpers navHelpers;
+
+    @Before
+    public void setUp() {
+        super.setup();  // Call the setup method from BaseTest
+        navHelpers = new AppNavigationHelpers(driver);  // Initialize helper class
+    }
+
+    @Test
+    public void testSignupFlow() {
+        navHelpers.navigateToSignup();
+
+        // Now on the signUp screen; locate fields and fill them out
+        WebElement userNameField = driver.findElement(AppiumBy.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[1]"));
+        WebElement passwordField = driver.findElement(AppiumBy.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[2]"));
+        WebElement confirmPasswordField = driver.findElement(AppiumBy.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.EditText[3]"));
+        WebElement signupButton = driver.findElement(AppiumBy.xpath("//android.view.View[@content-desc=\"Register\"]"));
+
+        userNameField.sendKeys("Test User");
+        passwordField.sendKeys("securepassword");
+        signupButton.click();
+
+        System.out.println("Account Created Successfully");
+        // Add an assertion to verify that signUp was successful
+   //     WebElement successMessage = driver.findElement(AppiumBy.id("com.example.mbl:id/successMessage"));
+    //    assertTrue("Signup success message is displayed", successMessage.isDisplayed());
+    }
 }
